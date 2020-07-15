0027fd29bc0488058ef79e3448bf11918cbec2a6
AdamWeissman <adam@comedytragedyepic.com>
2020-07-15 14:30:20 -0400

    weirdness with external cdn, until I restarted NPM... something to be aware of

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+
+const seasonConfig = {
+  summer: {
+    text: "let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: "burrr",
+    iconName: 'snowflake'
+  }
+};
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? 'summer' : 'winter';
@@ -11,15 +23,18 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach"
-
-  console.log(season)
+  // const text = season === 'winter' ? "Burr, it's chilly" : "Let's hit the beach";
+  // //const icon = season === 'winter' ? 'snowflake' : 'sun';
+  const { text, iconName } = seasonConfig[season]; 
 
   return (
     <div>
+      <h2>{`${iconName} icon`}</h2>
       <h1>{text}</h1>
     </div>
   );
-};
+}; 
 
 export default SeasonDisplay;
+
+
